aca5b520f99a6341689e88b755bb450cb63f4c17
rahulbotz-srm <rahulbotzofficial@gmail.com>
2025-05-18 02:27:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'NOT AVAILABLE RIGHT NOW ')
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://srmbossbot:YhS3OCHOb6RjTiXO@cluster0.xzgza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://rahulbotzofficial:rahulbotzofficial@cluster0.oufrcfu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files')
 
